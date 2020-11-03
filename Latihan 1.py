9c4a42c6fa768af079f1acb7f124597ee3ab3493
SultanFazalika <sultanfazalika6@gmail.com>
2020-11-03 22:06:32 +0700

    latihan7

---
+++ Latihan 1.py
@@ -0,0 +1,5 @@
+for i in range(10): #angka i berjumlah 10 angka
+   for j in range(10): #angka j berjumlah 10 angka
+      k = i+j #rumus dimana angka i+j, agar setiap pengulangan baris baru dimulai dengan angka +1
+      print(k, end=' ') #print 10 angka memanjang dan memberi spasi setiap print angka baru
+   print() #print ulang setelah 10 angka
---
+++ Latihan 2.py
@@ -0,0 +1,6 @@
+print("Masukan nilai N: 5")
+import random
+jumlah = 5
+for x in range(jumlah):
+    i = random.uniform(.0,.5)
+    print(i)
